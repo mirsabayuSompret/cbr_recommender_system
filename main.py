3b570a54f03ba446b860009f39280b7534627e0e
mirsabayu-xelodigital <mirsabayu@xelodigital.com>
2025-11-28 14:14:27 +0700

    initialize repository

---
+++ main.py
@@ -0,0 +1,11 @@
+class main():
+    def __init__(self):
+        print("Main class initialized")
+
+    def run(self):
+        print("Main class running")
+
+
+if __name__ == "__main__":
+    app = main()
+    app.run()
